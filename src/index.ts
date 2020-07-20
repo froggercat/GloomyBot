c8c3a899fc406a4ac049de8c2eab755e75454ea9
Sondra Miller <froggercat@gmail.com>
2020-07-20 16:16:38 -0400

    hello bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello")
+require('dotenv').config(); // Recommended way of loading dotenv
+import container from "./inversify.config";
+import { TYPES } from "./types";
+import { Bot } from "./bot";
+let bot = container.get<Bot>(TYPES.Bot);
+bot.listen().then(() => {
+    console.log('Logged in!')
+}).catch((error) => {
+    console.log('Oh no! ', error)
+});
